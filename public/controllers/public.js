ef3d299e5dfc0dbc3230281894f2f9496e48650b
Jeremy <JDraxler@gmail.com>
2016-12-01 21:51:23 +0000

    adding public endpoints

--- public/controllers/public.js
+++ public/controllers/public.js
@@ -3,11 +3,22 @@
 */
 
 exports.index = function(req, res) {
-    res.locals.msg = "test";
+    res.locals.msg = "HOME";
     res.render('home');
+};
+
+exports.guilds = function(req, res) {
+    res.locals.msg = "GUILDS";
+    res.render('guilds');
 };
 
 exports.heroes = function(req, res) {
     res.locals.msg = "HEROES";
     res.render('heroes');
 };
+
+exports.items = function(req, res) {
+    res.locals.msg = "ITEMS";
+    res.render('items');
+};
+
