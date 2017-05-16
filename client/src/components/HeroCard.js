784664ab8c706ed9cd2565a2aebea9c36876b735
Jeremy <JDraxler@gmail.com>
2017-05-16 20:13:00 +0000

    adding hero filter and streamlined hero layout

--- client/src/components/HeroCard.js
+++ client/src/components/HeroCard.js
@@ -7,41 +7,37 @@ class HeroCard extends React.Component {
     const hero = this.props.hero
     
     return (
-      <div key={hero._id} className="col-md-6 col-xs-12" >
-            
-        <div className="row hero-row">
-        
-          <div className="col-md-6 col-xs-12">
-            {(hero.image)?
-            <img src={hero.image.replace(/^http?:/i, "")} className="img-responsive" alt={hero.name}/>
-            : <img src={require('../../public/images/BlankCard-Hero.jpg')} className="img-responsive" alt={hero.name}/>
-            }
-          </div>
-          
-          <div className="col-md-6 col-xs-12">
-            <div className="panel panel-default">
-              <div className="panel-heading">{hero.name}</div>
-              <table className="table table-condensed table-bordered hero-info">
-                  <tbody>
-                    <tr>
-                      <td>Defense: {hero.defense}</td>
-                      <td>Health: {hero.health}</td>
-                    </tr>
-                    <tr>
-                      <td colSpan="2" className="ability-info">
-                        <strong><em>{hero.ability}</em></strong>
-                        <div>{hero.effect}</div>
-                      </td>
-                    </tr>
-                  </tbody>
-              </table>
-            </div>
-          </div>
-          
+      <div key={hero._id} className="col-md-3 col-xs-12" >
+           
+        <div className="panel panel-default text-center">
+          <div className="panel-heading text-center"><strong>{hero.name}</strong></div>
+          <table className="table table-condensed table-bordered hero-info">
+              <tbody>
+                <tr>
+                  <td colSpan="2">
+                    {(hero.image)?
+                    <img src={hero.image.replace(/^http?:/i, "")} className="img-responsive" alt={hero.name}/>
+                    : <img src={require('../../public/images/BlankCard-Hero.jpg')} className="img-responsive" alt={hero.name}/>
+                    }
+                  </td>
+                </tr>
+                <tr>
+                  <td>Defense: {hero.defense}</td>
+                  <td>Health: {hero.health}</td>
+                </tr>
+                <tr>
+                  <td colSpan="2" className="ability-info" >
+                    <strong><em>{hero.ability}</em></strong>
+                    <div>{hero.effect}</div>
+                  </td>
+                </tr>
+              </tbody>
+          </table>
         </div>
+          
               
         </div>
-    );
+    )
   };
 }
 
--- client/src/components/Heroes.js
+++ client/src/components/Heroes.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import { fetchHeroes} from '../actions/heroActions';
 import HeroCard from './HeroCard';
 import Loading from './Loading'
+import HeroSetDropdown from './forms/fields/HeroSetDropdown'
 
 function mapStateToProps(store) {
   return { 
@@ -14,8 +15,22 @@ function mapStateToProps(store) {
 
 class Heroes extends React.Component {
   
+  constructor(props){
+    super(props);
+    this.state = {
+      filter: false,
+      sort: false,
+    };
+  }
+  
   componentWillMount() {
     this.props.dispatch(fetchHeroes());
+  }
+  
+  handleChange(event) {
+    if(event.target.value === "" || !event.target.value)
+      this.setState({filter: false});
+    this.setState({filter: event.target.value});
   }
   
   render() {
@@ -23,17 +38,25 @@ class Heroes extends React.Component {
     if(!this.props.heroesFetched)
       return <Loading title="Heroes"/>
     
-    const Heroes = this.props.heroes.map((hero, index)=>{
+    const Heroes = this.props.heroes.filter((hero, i)=>{
+      if(this.state.filter)
+        return hero.set === this.state.filter
+      return true
+    })
+    .map((hero, index)=>{
       return <HeroCard hero={hero} key={hero._id} />
-    });
+    })
+    
     
     // Dirty way to get grid to render properly
     var Rows = []
-    for(var i = 0; i < Heroes.length; i+=2){
+    for(var i = 0; i < Heroes.length; i+=4){
       Rows.push(
         <div className="row" key={i}>
           {Heroes[i]}
           {Heroes[i+1]}
+          {Heroes[i+2]}
+          {Heroes[i+3]}
         </div>
       )
     }
@@ -41,6 +64,12 @@ class Heroes extends React.Component {
     return (
       <div>
         <h2>Heroes</h2>
+        <div className="row form-inline">
+          <div className="col-md-12">
+            <strong>Filter </strong>
+            <HeroSetDropdown handleChange={this.handleChange.bind(this)}/>
+          </div>
+        </div>
         {Rows}
       </div>
     );
