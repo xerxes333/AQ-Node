8eb45319c602e85fdfc0e413e1be05a005bbc020
Jeremy <JDraxler@gmail.com>
2017-05-17 20:27:25 +0000

    adding mobile friendly layout

--- client/src/components/Campaigns.js
+++ client/src/components/Campaigns.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-router';
+import { Link, browserHistory  } from 'react-router';
 import { connect } from "react-redux";
 
 import { fetchCampaigns } from '../actions/campaignActions';
@@ -16,6 +16,10 @@ class Campaigns extends React.Component {
   
   componentWillMount() {
     this.props.dispatch(fetchCampaigns());
+  }
+  
+  rowClicked(id){
+    browserHistory.push(`/campaigns/${id}`);
   }
   
   render() {
@@ -42,33 +46,52 @@ class Campaigns extends React.Component {
           </td>
         </tr>
       });
-    
+      
+      const CampaignRowsSmall = (!campaigns.length)? 
+      <tr><td colSpan="4">No Campaigns</td></tr> : 
+      campaigns.map((campaign)=>{
+        return <tr key={campaign._id} onClick={() => this.rowClicked(campaign._id)}>
+          <td>
+            <img src={require('../../public/images/logo-arcadia-quest.png')} className="img-responsive guild-name-icon-48" alt="logo"/>
+            {campaign.name}
+          </td>
+        </tr>
+      });
+     
     return (
       <div>
         
         <h2>
           Campaigns
           <Link to={`/campaigns/new`} className="btn btn-primary new-campaign">
-            <span className="glyphicon glyphicon-plus-sign" style={{paddingRight: "10px"}}></span>
-            New
+            <span className="glyphicon glyphicon-plus-sign" style={{paddingRight: "10px"}}></span>New
           </Link>
         </h2>
         
-        <div className="table-responsive">
-        <table className="table table-striped table-bordered">
-          <thead>
-            <tr>
-              <td>Name</td>
-              <td>Description</td>
-              <td>Created</td>
-              <td>View</td>
-            </tr>
-          </thead>
-          <tbody>
-            {CampaignRows}
-          </tbody>
-        </table>
-        </div>
+        { (window.innerWidth < 768)? 
+          <table className="table table-striped table-bordered" style={{fontSize: "18px"}}>
+            <tbody>
+              {CampaignRowsSmall}
+            </tbody>
+          </table>
+          
+          :<table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <td>Name</td>
+                <td>Description</td>
+                <td>Created</td>
+                <td>View</td>
+              </tr>
+            </thead>
+            <tbody>
+              {CampaignRows}
+            </tbody>
+          </table>
+        }
+        
+        
+        
         
       </div>
     );
