4ac91585c13c94c9ed5d4bd91045d62c7cfb768e
Jeremy <JDraxler@gmail.com>
2017-10-02 18:17:56 +0000

    updates the hero info layout

--- client/src/components/GuildHeroInfo.js
+++ client/src/components/GuildHeroInfo.js
@@ -7,49 +7,48 @@ class GuildHeroInfo extends React.Component {
       
       if(!hero.icon) hero.icon = "none.png"
       
-      const lvl = (hero.level && hero.level > 1) ? <sup>{hero.level}</sup> : "";
+      const lvl = (hero.level && hero.level > 1) ? <sup>{hero.level.toString()}</sup> : "";
       
       return (
         <div className="row">
           <div className="col-md-12">
             
             {hasHero && 
-            <table className="table table-bordered guild-hero-info">
-            
-              {!isEditing && <thead><tr><th colSpan="2">{hero.name}{lvl}</th></tr></thead> }
-              
-              <tbody>
-              <tr>
-                <td className="effect">{hero.effect}</td>
-                <td className="portrait-cell">
-                  
-                  <table className="guild-hero-portrait">
+              <div className="panel panel-default panel-hero">
+                
+                <div className="panel-heading">
+                  {!isEditing && <div>{hero.name} {lvl}</div> }
+                </div>
+                
+                <div className="panel-body">
+                  <table className="hero-info">
                     <tbody>
-                    <tr>
-                      <td colSpan="2"> <img src={`images/heroes/icons/${hero.icon}`} className="img-responsive" alt={hero.name} /> </td>
-                    </tr>
-                    <tr>
-                      <td> 
-                        <div className="icons">
-                          <img src={require('../../public/images/def.png')} className="img-responsive" alt="defense"/> 
-                          <span className="hero-def">{hero.defense}</span>
-                        </div>
-                      </td>
-                      <td> 
-                        <div className="icons">
-                          <img src={require('../../public/images/hp.png')} className="img-responsive" alt="health"/>
-                          <span className="hero-hp">{hero.health}</span>
-                        </div>
-                      </td>
-                    </tr>
+                      <tr>
+                      
+                        <td className="effect" >
+                          {hero.effect}
+                        </td>
+                        
+                        <td className="portrait-cell">
+                          <img src={`images/heroes/icons/${hero.icon}`} className="img-responsive portrait" alt={hero.name} />
+                          
+                          <div className="icons">
+                            <img src={require('../../public/images/def.png')} className="img-responsive" alt="defense"/> 
+                            <span className="hero-def">{hero.defense}</span>
+                          </div>
+                        
+                          <div className="icons">
+                            <img src={require('../../public/images/hp.png')} className="img-responsive" alt="health"/>
+                            <span className="hero-hp">{hero.health}</span>
+                          </div>
+                        </td>
+                
+                      </tr>
                     </tbody>
                   </table>
-            
-                </td>
-              </tr>
-              </tbody>
-              
-            </table>
+                </div>
+                
+              </div>
             }
             
         
